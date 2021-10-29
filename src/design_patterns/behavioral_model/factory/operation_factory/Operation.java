6db400185035c4051001b9f34a60fc333374f24a
zhaoqiyi <627844735@qq.com>
2021-10-29 18:42:49 +0800

    新增工厂-计算机

---
+++ src/design_patterns/behavioral_model/factory/operation_factory/Operation.java
@@ -0,0 +1,42 @@
+package design_patterns.behavioral_model.factory.operation_factory;/**
+ * Created by Enzo Cotter on 2021/10/29.
+ */
+
+/**
+ * @author:zqy
+ * @date:2021/10/29 17:43
+ * @desc:
+ */
+public  class Operation {
+
+    private Double numberOne;       //操作数1
+
+    private Double numberTwo;       //操作数2
+
+    private String operator;        //操作符
+
+
+    public Double getNumberOne() {
+        return numberOne;
+    }
+
+    public void setNumberOne(Double numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public Double getNumberTwo() {
+        return numberTwo;
+    }
+
+    public void setNumberTwo(Double numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
